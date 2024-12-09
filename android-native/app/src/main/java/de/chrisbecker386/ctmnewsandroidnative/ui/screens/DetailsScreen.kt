63f8b6f5da61a42b25adaa55fe3add0aa5f50694
Christopher Becker <christopherbecker@online.de>
2024-12-09 10:18:26 +0100

    add basic navigation

---
+++ android-native/app/src/main/java/de/chrisbecker386/ctmnewsandroidnative/ui/screens/DetailsScreen.kt
@@ -0,0 +1,20 @@
+package de.chrisbecker386.ctmnewsandroidnative.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DetailsScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Details Screen")
+    }
+}
